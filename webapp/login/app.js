5f90ced18c9d698daed1a66e45e1136be8f53e66
Pandy-hull <dab.bomb@icloud.com>
2026-04-02 21:48:58 -0400

    the main site

---
+++ webapp/login/app.js
@@ -0,0 +1,12 @@
+function alertMessage(msg) {
+    alert(msg);
+}
+
+function showTime(id) {
+    if (id == 'dumbass') {
+        document.getElementById(id).innerHTML = "FUCK YOUUUUUUUUUUU"
+    }
+    else {
+        document.getElementById(id).innerHTML = Date()
+    }
+}
